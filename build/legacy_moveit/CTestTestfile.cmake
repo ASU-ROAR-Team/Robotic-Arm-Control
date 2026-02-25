ea309854d8230d5c22f650b5d03701931d2fbce8
ahmed-2114 <yjbnw9jnz8@privaterelay.appleid.com>
2026-02-25 03:46:56 +0200

    legacy: sync full workspace from old_ROAR_arm

---
+++ build/legacy_moveit/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/roar/new_arm/old_ROAR_arm/src/legacy_moveit
+# Build directory: /home/roar/new_arm/old_ROAR_arm/build/legacy_moveit
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ build/legacy_pkg/CMakeFiles/legacy_pkg_uninstall.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/legacy_pkg_uninstall"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/legacy_pkg_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
